1a68bdedda9c9d16f8bbd10af1d9b9b0035ce375
Mark Bosmediano <markbosme123@gmail.com>
2025-11-14 00:36:42 -0500

    First iteration, basic layout created. Will add more things as they occur to me

--- markb-website/next.config.ts
+++ markb-website/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Ensure we're using the "experimental" app dir support for Next-on-Pages if needed
+  experimental: {
+    // if using app router:
+    appDir: true
+  }
 };
 
 export default nextConfig;
