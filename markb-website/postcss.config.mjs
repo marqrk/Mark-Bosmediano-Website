c60f3e8b78f933026f4050e621a376fc5c760239
Mark Bosmediano <markbosme123@gmail.com>
2025-11-13 23:56:48 -0500

    First iteration, basic layout created. Will add more things as they occur to me

--- markb-website/postcss.config.mjs
+++ markb-website/postcss.config.mjs
@@ -1,10 +1,7 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
- if (process.env.NODE_ENV === 'development') {
-   await setupDevPlatform();
- }
-
-export default nextConfig;
+export default config;
