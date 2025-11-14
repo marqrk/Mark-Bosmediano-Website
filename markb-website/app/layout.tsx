830987301f7dbb9b3880fb93786d73d401c75116
Mark Bosmediano <markbosme123@gmail.com>
2025-11-14 01:55:26 -0500

    First iteration, basic layout created. Will add more things as they occur to me

--- markb-website/app/layout.tsx
+++ markb-website/app/layout.tsx
@@ -1,18 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-import SideNav from '@/app/ui/dashboard/sidenav';
+import SideNav from "@/app/ui/dashboard/sidenav";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const sans = Inter({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const mono = Roboto_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -21,20 +20,18 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-   <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-          >
-              <div className="w-full flex-none md:w-64">
-                  <SideNav />
-              </div>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${sans.variable} ${mono.variable} antialiased`}>
+                <div className="w-full flex-none md:w-64">
+                    <SideNav />
+                </div>
+                {children}
+            </body>
+        </html>
+    );
 }
